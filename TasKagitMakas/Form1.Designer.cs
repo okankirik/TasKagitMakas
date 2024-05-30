bd6c35bc051d2d75a523f038b9c1549206403cfa
Okan Kırık <okankirik@gmail.com>
2024-05-30 16:06:51 +0300

    Add project files.

---
+++ TasKagitMakas/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace TasKagitMakas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnTas = new System.Windows.Forms.Button();
+            this.btnKagit = new System.Windows.Forms.Button();
+            this.btnMakas = new System.Windows.Forms.Button();
+            this.btnMakas2 = new System.Windows.Forms.Button();
+            this.btnKagit2 = new System.Windows.Forms.Button();
+            this.btnTas2 = new System.Windows.Forms.Button();
+            this.lblScore2 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnTas
+            // 
+            this.btnTas.BackColor = System.Drawing.Color.White;
+            this.btnTas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTas.Location = new System.Drawing.Point(326, 268);
+            this.btnTas.Name = "btnTas";
+            this.btnTas.Size = new System.Drawing.Size(148, 46);
+            this.btnTas.TabIndex = 0;
+            this.btnTas.Text = "Taş";
+            this.btnTas.UseVisualStyleBackColor = false;
+            this.btnTas.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnKagit
+            // 
+            this.btnKagit.BackColor = System.Drawing.Color.White;
+            this.btnKagit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKagit.Location = new System.Drawing.Point(326, 330);
+            this.btnKagit.Name = "btnKagit";
+            this.btnKagit.Size = new System.Drawing.Size(148, 46);
+            this.btnKagit.TabIndex = 1;
+            this.btnKagit.Text = "Kağıt";
+            this.btnKagit.UseVisualStyleBackColor = false;
+            this.btnKagit.Click += new System.EventHandler(this.btnKagit_Click);
+            // 
+            // btnMakas
+            // 
+            this.btnMakas.BackColor = System.Drawing.Color.White;
+            this.btnMakas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnMakas.Location = new System.Drawing.Point(326, 392);
+            this.btnMakas.Name = "btnMakas";
+            this.btnMakas.Size = new System.Drawing.Size(148, 46);
+            this.btnMakas.TabIndex = 2;
+            this.btnMakas.Text = "Makas";
+            this.btnMakas.UseVisualStyleBackColor = false;
+            this.btnMakas.Click += new System.EventHandler(this.btnMakas_Click);
+            // 
+            // btnMakas2
+            // 
+            this.btnMakas2.BackColor = System.Drawing.Color.White;
+            this.btnMakas2.Enabled = false;
+            this.btnMakas2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnMakas2.Location = new System.Drawing.Point(326, 136);
+            this.btnMakas2.Name = "btnMakas2";
+            this.btnMakas2.Size = new System.Drawing.Size(148, 46);
+            this.btnMakas2.TabIndex = 5;
+            this.btnMakas2.Text = "Makas";
+            this.btnMakas2.UseVisualStyleBackColor = false;
+            // 
+            // btnKagit2
+            // 
+            this.btnKagit2.BackColor = System.Drawing.Color.White;
+            this.btnKagit2.Enabled = false;
+            this.btnKagit2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKagit2.Location = new System.Drawing.Point(326, 74);
+            this.btnKagit2.Name = "btnKagit2";
+            this.btnKagit2.Size = new System.Drawing.Size(148, 46);
+            this.btnKagit2.TabIndex = 4;
+            this.btnKagit2.Text = "Kağıt";
+            this.btnKagit2.UseVisualStyleBackColor = false;
+            // 
+            // btnTas2
+            // 
+            this.btnTas2.BackColor = System.Drawing.Color.White;
+            this.btnTas2.Enabled = false;
+            this.btnTas2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTas2.Location = new System.Drawing.Point(326, 12);
+            this.btnTas2.Name = "btnTas2";
+            this.btnTas2.Size = new System.Drawing.Size(148, 46);
+            this.btnTas2.TabIndex = 3;
+            this.btnTas2.Text = "Taş";
+            this.btnTas2.UseVisualStyleBackColor = false;
+            // 
+            // lblScore2
+            // 
+            this.lblScore2.AutoSize = true;
+            this.lblScore2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblScore2.Location = new System.Drawing.Point(606, 19);
+            this.lblScore2.Name = "lblScore2";
+            this.lblScore2.Size = new System.Drawing.Size(121, 39);
+            this.lblScore2.TabIndex = 6;
+            this.lblScore2.Text = "Score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(754, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 39);
+            this.label2.TabIndex = 7;
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(330, 204);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(0, 39);
+            this.lblDurum.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(754, 392);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 39);
+            this.label1.TabIndex = 10;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblScore.Location = new System.Drawing.Point(606, 392);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(121, 39);
+            this.lblScore.TabIndex = 9;
+            this.lblScore.Text = "Score:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblScore2);
+            this.Controls.Add(this.btnMakas2);
+            this.Controls.Add(this.btnKagit2);
+            this.Controls.Add(this.btnTas2);
+            this.Controls.Add(this.btnMakas);
+            this.Controls.Add(this.btnKagit);
+            this.Controls.Add(this.btnTas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTas;
+        private System.Windows.Forms.Button btnKagit;
+        private System.Windows.Forms.Button btnMakas;
+        private System.Windows.Forms.Button btnMakas2;
+        private System.Windows.Forms.Button btnKagit2;
+        private System.Windows.Forms.Button btnTas2;
+        private System.Windows.Forms.Label lblScore2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TasKagitMakas/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TasKagitMakas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int score = 0;
+        int bot = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnTas2.BackColor = Color.White;
+            btnKagit2.BackColor = Color.White;
+            btnMakas2.BackColor = Color.White;
+            Random random = new Random();
+            int deger;
+            deger = random.Next(1, 4);
+
+            if (deger == 1)
+            {
+                btnTas2.BackColor = Color.Yellow;
+                lblDurum.Text = "Berabere";
+            }
+            else if (deger == 2)
+            {
+                bot++;
+                label2.Text = bot.ToString();
+                btnKagit2.BackColor = Color.DarkRed;
+                lblDurum.Text = "Kaybettiniz";
+            }
+            else if (deger == 3)
+            {
+                score++;
+                label1.Text = score.ToString();
+                btnMakas2.BackColor = Color.Green;
+                lblDurum.Text = "Kazandınız";
+            }
+        }
+
+        private void btnKagit_Click(object sender, EventArgs e)
+        {
+            btnTas2.BackColor = Color.White;
+            btnKagit2.BackColor = Color.White;
+            btnMakas2.BackColor = Color.White;
+            Random random = new Random();
+            int deger;
+            deger = random.Next(1, 4);
+
+            if (deger == 1)
+            {
+                score++;
+                label1.Text = score.ToString();
+                btnTas2.BackColor = Color.Green;
+                lblDurum.Text = "Kazandınız";
+            }
+            else if (deger == 2)
+            {
+                btnKagit2.BackColor = Color.Yellow;
+                lblDurum.Text = "Berabere";
+            }
+            else if (deger == 3)
+            {
+                bot++;
+                label2.Text = bot.ToString();
+                btnMakas2.BackColor = Color.DarkRed;
+                lblDurum.Text = "Kaybettiniz";
+            }
+        }
+
+        private void btnMakas_Click(object sender, EventArgs e)
+        {
+            btnTas2.BackColor = Color.White;
+            btnKagit2.BackColor = Color.White;
+            btnMakas2.BackColor = Color.White;
+            Random random = new Random();
+            int deger;
+            deger = random.Next(1, 4);
+
+            if (deger == 1)
+            {
+                bot++;
+                label2.Text = bot.ToString();
+                btnTas2.BackColor = Color.DarkRed;
+                lblDurum.Text = "Kaybettiniz";
+            }
+            else if (deger == 2)
+            {
+                score++;
+                label1.Text = score.ToString();
+                btnKagit2.BackColor = Color.Green;
+                lblDurum.Text = "Kazandınız";
+            }
+            else if (deger == 3)
+            {
+                btnMakas2.BackColor = Color.Yellow;
+                lblDurum.Text = "Berabere";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (score == 3)
+            {
+                lblDurum.Text = "Kazandınız";
+                btnKagit.Enabled = false;
+                btnKagit2.Enabled = false;
+                btnMakas.Enabled = false;
+                btnMakas2.Enabled = false;
+                btnTas.Enabled = false;
+                btnTas2.Enabled = false;
+                
+            }
+            else if (bot == 3)
+            {
+                lblDurum.Text = "Kaybettiniz";
+                btnKagit.Enabled = false;
+                btnKagit2.Enabled = false;
+                btnMakas.Enabled = false;
+                btnMakas2.Enabled = false;
+                btnTas.Enabled = false;
+                btnTas2.Enabled = false;
+            }
+        }
+    }
+}
